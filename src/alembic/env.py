ce7bc0dc79ef080426649243386c6b3095a4241b
mbgoodguy <nerooren4001@gmail.com>
2024-04-07 13:35:39 +0300

    Complete upgrade head. First revision succesfully accepted

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -5,9 +5,14 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from src.config import settings
+from src.db import Base
+from src.candies.models import Candies
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", f"{settings.DB_URL}")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +23,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
